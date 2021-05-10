14e43fe415daf516c85f3a23e0e440616526ac78
enricoratuwongo <enricowtf@gmail.com>
2021-05-10 11:15:41 +0800

    final test

---
+++ FinalTest/Keypad.cpp
@@ -0,0 +1,13 @@
+// Keypad.cpp
+// Member-function definition for class Keypad (the ATM's keypad).
+#include <iostream>
+using std::cin;
+
+#include "Keypad.h" // Keypad class definition
+
+// return an integer value entered by user
+int Keypad::getInput() const{
+	int input; // variable to store the input
+	cin >> input; // we assume that user enters an integer
+	return input; // return the value entered by user
+} // end function getInput
